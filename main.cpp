d2c417657f33a63ab67ea5e9839badea99849524
Rafael <rafac@unitec.edu>
2020-03-13 13:01:36 -0600

    Para inicializar el Repo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int op;
+    cin >> op;
+    return op;
+}
+
+void selector(){}
+
+int main(){
+    selector();
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+salida: main.cpp
+	g++ -o salida main.cpp
+
+clean:
+	rm -f *.o salida core
